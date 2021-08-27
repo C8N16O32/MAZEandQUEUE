cbd21a3bd0c8111635d7d20e731559f42797d1be
C8N16O32 <44058522+C8N16O32@users.noreply.github.com>
2021-08-27 12:37:29 +0800

    支持多起点1终点 支持打印结果

--- 2.0.cpp
+++ 2.0.cpp
@@ -1,4 +1,5 @@
-#include "stdio.h" 
+#include "stdio.h"
+
 //循环队列
 template<class T>
 class QUEUE {
@@ -30,17 +31,15 @@ public:
 		if (size < m_capacity)changesize(size);
 		return;
 	};
-	//push_end
+	//入队/出队
 	void push_end(T&inp) {
 		checkcapa(1);
 		v[m_end%m_capacity] = inp; m_end++;
 		return;
 	};
-	//head
 	T&head() {
 		return v[m_head];
 	};
-	//pop_head
 	void pop_head() {
 		m_head++;
 		if (m_head == m_capacity) {
@@ -61,8 +60,7 @@ template<class T,typename K>
 class LIST {
 public:
 	struct N {
-		K key;
-		T data;
+		K key; T data;
 		N*next = NULL, *prev = NULL;
 	};
 private:
@@ -82,7 +80,7 @@ public:
 		}; 
 		m_head = newhead; 
 	};
-	//插入
+	//增查删
 	void insert(N *px, N *ppre = NULL) {
 		//首节点
 		if (ppre == NULL) {
@@ -96,8 +94,7 @@ public:
 		};
 		m_size++;
 		return;
-	};
-	//查找
+	};	
 	N*search(K key) {
 		if (isempty())return NULL;
 		N *cur = m_head;
@@ -107,7 +104,6 @@ public:
 		};
 		return NULL;
 	};
-	//删除
 	void erase(N *px) {
 		//最后一个节点
 		if (m_size == 1) {
@@ -137,23 +133,21 @@ public:
 	using Q = QUEUE<T>;
 	LIST<Q, int> list;
 	using N = typename LIST<Q, int>::N;
-	void push_queue(N*note, T &data) {
-		note->data.push_end(data);
-	};
-	void push_list(T &data, int key) {
-		N *temp = new N;
-		temp->data.push_end(data); temp->key = key;
-		if (list.isempty()) { list.sethead(temp); return; };
-		int size = list.size();
-		N *cur = list.head();
-		int i; for (i = 0; i < size; i++) {
-			if (key < cur->key)break;
-			cur = cur->next;
-		};
-		list.insert(temp, cur->prev);
-		if (i == 0 && cur == list.head())list.sethead(temp);
-	};
 	void push(T &data, int count) {
+		auto push_queue = [&](N*note, T &data) {note->data.push_end(data); };
+		auto push_list=[&](T &data, int key) {
+			N *temp = new N;
+			temp->data.push_end(data); temp->key = key;
+			if (list.isempty()) { list.sethead(temp); return; };
+			int size = list.size();
+			N *cur = list.head();
+			int i; for (i = 0; i < size; i++) {
+				if (key < cur->key)break;
+				cur = cur->next;
+			};
+			list.insert(temp, cur->prev);
+			if (i == 0 && cur == list.head())list.sethead(temp);
+		};
 		N*s = list.search(count);
 		if (s)push_queue(s, data);
 		else push_list(data, count);
@@ -181,6 +175,7 @@ public:
 			int bf_count = 0, bf_tag = 0, p_tag = 0;//bfs计数 bfs标记 寻找路径时的计数
 		}**v = NULL;
 	}maze;
+	//输入/输出
 	void setmazesizeandnew(int m, int n, int si, int sj, int ei, int ej) {
 		maze.m = m; maze.n = n; maze.start = { sj,si }; maze.end = { ej, ei };
 		maze.v = new MAZE::CELL*[m];
@@ -201,28 +196,33 @@ public:
 		auto around = [](int ord, T&cur)->T {static int offx[] = { 0,1,0,-1 }, offy[] = { -1,0,1,0 }; return{ offx[ord] + cur.x,offy[ord] + cur.y }; };
 		auto checkandmark = [&](T&temp, int countcur)->bool {if (temp.x < 0 || temp.x >= maze.n || temp.y < 0 || temp.y >= maze.m)return 0;
 		MAZE::CELL&cell = maze.v[temp.y][temp.x]; if (cell.ba_tag == 1 || cell.bf_tag == 1)return 0; cell.bf_tag = 1; cell.bf_count = countcur + cell.ba_cost; return 1; };
+		auto isstart = [&](T&temp)->bool {if (maze.v[temp.y][temp.x].ba_tag == 2)return 1; return 0; };
 		auto isend = [&](T&temp)->bool {if (temp.x == maze.end.x&&temp.y == maze.end.y)return 1; return 0; };
 		auto calcurcount = [&](T&cur) {MAZE::CELL&cell = maze.v[cur.y][cur.x]; return cell.bf_count; };
-		issolved = 1;
+		issolved = 0;
 		PRIQUE<T> ts; T cur, temp; int isempty = 0, curcount;
-		cur = maze.start;//起点
+		//由于有多个起点 从终点往起点找
+		cur = maze.end;
 		checkandmark(cur, -1);
 		ts.push(cur, 0);
 		while (1) {
-			cur = ts.pop(&isempty); if (isempty)break;
+			cur = ts.pop(&isempty);
+			if (isempty) break;
 			curcount = calcurcount(cur);
 			for (int i = 0; i < 4; i++) {
 				temp = around(i, cur);
 				if (checkandmark(temp, curcount)) {
-					if (isend(temp))return curcount + 1;
+					if (isstart(temp)) {
+						issolved = 1;
+						return curcount + 1;
+					}
 					ts.push(temp, curcount);
 				};
 			};
 		};
-		issolved = 0;
 		return 0;
 	};
-	//计算最短路径
+	//根据涂色生成路径
 	int pathmain(bool &issolved) {
 		auto searchthelargestcount = [&](T&max)->int {
 			int isempty = 1;
@@ -239,20 +239,37 @@ public:
 			if (isempty)return 1;
 			max = { maxj,maxi }; return 0;
 		};
-		auto isend = [&](T&temp)->bool {if (temp.x == maze.start.x&&temp.y == maze.start.y)return 1; return 0; };
+		auto searchthelargestcount_alsoisatart = [&](T&max)->int {
+			int isempty = 1;
+			int maxc = -1, maxi, maxj;
+			MAZE::CELL*temp = NULL;
+			for (int i = 0; i < maze.m; i++)
+				for (int j = 0; j < maze.n; j++){
+					temp = &maze.v[i][j];
+					if (temp->ba_tag == 2 && temp->bf_tag&&maxc < maze.v[i][j].bf_count) {
+						if (isempty)isempty = 0;
+						maxc = maze.v[i][j].bf_count; maxi = i; maxj = j;
+					}
+				}
+			if (isempty)return 1;
+			max = { maxj,maxi }; return 0;
+		};
+		auto isstart = [&](T&temp)->bool {if (maze.v[temp.y][temp.x].ba_tag == 2)return 1; return 0; };
+		auto isend = [&](T&temp)->bool {if (temp.x == maze.end.x&&temp.y == maze.end.y)return 1; return 0; };
 		T max;
 		if (issolved == 0) {
 			int isempty = searchthelargestcount(max);
-			if (isempty||isend(max))return 0;
+			if (isempty||isstart(max))return 0;
 		}
-		else max = maze.end;
+		else searchthelargestcount_alsoisatart(max);
+
 		auto mark = [&](T&temp) { maze.v[temp.y][temp.x].p_tag = 1; };
 		auto around = [](int ord, T&cur)->T {static int offx[] = { 0,1,0,-1 }, offy[] = { -1,0,1,0 }; return{ offx[ord] + cur.x,offy[ord] + cur.y }; };
 		auto check = [&](T&temp)->bool {if (temp.x < 0 || temp.x >= maze.n || temp.y < 0 || temp.y >= maze.m)return 0;
-			MAZE::CELL&cell = maze.v[temp.y][temp.x]; if (cell.bf_tag == 0)return 0; return 1; };
+		MAZE::CELL&cell = maze.v[temp.y][temp.x]; if (cell.bf_tag == 0 || cell.p_tag == 1)return 0; return 1; };
 		auto getcount = [&](T&temp)->int {return maze.v[temp.y][temp.x].bf_count; };
-		auto choose = [&](T&cur)->T {
-			T temp, minT; int f = 0, minc;
+		auto choose = [&](T&cur,int &f)->T {
+			T temp, minT; f = 0;int minc;
 			for (int i = 0; i < 4; i++) {
 				temp = around(i, cur);
 				if (check(temp)) {
@@ -262,12 +279,13 @@ public:
 			}
 			return minT;
 		};
-		issolved = 1;
+		issolved = 1; int ok = 1;
 		T cur = max;
 		do {
 			mark(cur);
-			cur = choose(cur);
-		} while (!isend(cur));
+			cur = choose(cur, ok);
+		} while (ok&&!isend(cur));
+		if (ok == 0)issolved = 0;
 		return issolved;
 	};
 	//构造/析构
@@ -283,16 +301,16 @@ public:
 	//数据
 	struct D { int x, y; };
 	struct MAZE {
-		int m, n; D stars, end;
+		int m, n; D start, end;
 		int **v = NULL;
 	}maze;
-	//代称、字符、是否可走、cost
+	//信息
 	//enum mazenum { 地面, 墙壁, 可破坏的墙壁1 };
-#define chnum 3
+#define chnum 4
 	char se[2] = { 'r','a' };//起点和终点
-	char ch[chnum] = { '.','#','x' };
-	int tag[chnum] = { 0,1,0 };
-	int cost[chnum] = { 1,0,1 + 1 };
+	char ch[chnum] = { '.','#','x','r' };
+	int tag[chnum] = { 0,1,0,2 };
+	int cost[chnum] = { 1,0,1 + 1,1 };
 	//创建、释放
 	void setandnew(int m, int n) {
 		maze.m = m; maze.n = n;
@@ -304,14 +322,15 @@ public:
 		for (int i = 0; i < m; i++)delete[]maze.v[i];
 		delete maze.v; maze.v = NULL;
 	};
+
 	//从控制台输入迷宫
 	void inputformscanf() {
 		auto set = [&](int i, int j, char c) {
+			if (c == se[0])maze.start = { j,i };
+			if (c == se[1])maze.end = { j,i };
 			for (int ci = 0; ci < chnum; ci++)
 				if (ch[ci] == c) { maze.v[i][j] = ci; return; }
 			maze.v[i][j] = 0;
-			if (c == se[0])maze.stars = { j,i };
-			if (c == se[1])maze.end = { j,i };
 		};
 		int m = maze.m, n = maze.n;
 		char *temp = new char[n];
@@ -324,7 +343,7 @@ public:
 		}
 		return;
 	};
-	//logistic生成迷宫
+	//利用logistic递推数列输入迷宫
 	void inputformlogistic(double a, double x0, double wall, double x, int si, int sj, int ei, int ej) {
 		double xnext; int m = maze.m, n = maze.n;
 		for (int i = 0; i < m; i++)for (int j = 0; j < n; j++) {
@@ -334,7 +353,8 @@ public:
 			else maze.v[i][j] = 0;
 			x0 = xnext;
 		}
-		maze.stars = { sj,si }; maze.end = { ej,ei };
+		maze.start = { sj,si }; maze.end = { ej,ei };
+		maze.v[si][sj] = 3;
 		return;
 	};
 
@@ -342,11 +362,12 @@ public:
 	MAZE_SOLVE maze_solve;
 	int solve_newsetandcalall(bool &issolved,bool &issolved2) {
 		int m = maze.m, n = maze.n;
-		maze_solve.setmazesizeandnew(m, n, maze.stars.y, maze.stars.x, maze.end.y, maze.end.x);
+		maze_solve.setmazesizeandnew(m, n, maze.start.y, maze.start.x, maze.end.y, maze.end.x);
 		for (int i = 0; i < m; i++)for (int j = 0; j < n; j++)
 		maze_solve.setmazecontent_single(i, j, cost[maze.v[i][j]], tag[maze.v[i][j]]);
 		int re = 0;
 		re = maze_solve.bfsmain(issolved);
+		//ACM的题目只需要时长 不需要具体路径
 		issolved2 = issolved;
 		maze_solve.pathmain(issolved2);
 		return re;
@@ -356,11 +377,58 @@ public:
 	};
 
 	//打印
-
+	void solve_plotcal(int ord = 0) {
+		auto choosechar = [&](int i, int j)->char {
+			MAZE_SOLVE::MAZE::CELL &cell = maze_solve.maze.v[i][j];
+			if (cell.bf_tag)return 'O';
+			return ch[maze.v[i][j]];
+		};
+		int m = maze.m, n = maze.n;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				printf("%c|", choosechar(i, j));
+			}
+			printf("\n");
+		}
+		printf("plot:%d\n\n", ord);
+	};
+	void solve_plotpath(int ord = 0) {
+		auto choosechar = [&](int i, int j)->char {
+			MAZE_SOLVE::MAZE::CELL &cell = maze_solve.maze.v[i][j];
+			if (cell.p_tag)return 'O';
+			return ch[maze.v[i][j]];
+		};
+		int m = maze.m, n = maze.n;
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				printf("%c|", choosechar(i, j));
+			}
+			printf("\n");
+		}
+		printf("plot:%d\n\n", ord);
+	};
 #undef chnum
 };
 
-//测试1 简单测试 从控制台输入 输出步数
+////测试1 简单测试 从控制台输入 输出步数
+////可能需要调整MAZE_MAIN::solve_newsetandcalall 取消计算具体路径以节省计算量
+//int main() {
+//	int m, n;
+//	MAZE_MAIN maze;
+//	int re; bool iss1, iss2;
+//	while (~scanf("%d%d", &m, &n)) {
+//		maze.setandnew(m, n);
+//		maze.inputformscanf();
+//		re = maze.solve_newsetandcalall(iss1, iss2);
+//		if (iss1)printf("%d\n", re);
+//		else printf("Poor ANGEL has to stay in the prison all his life.\n");
+//		maze.solve_del();
+//		maze.del();
+//	};
+//	return 0;
+//}
+
+//测试1p 简单测试 从控制台输入 输出步数 打印结果
 int main() {
 	int m, n;
 	MAZE_MAIN maze;
@@ -371,6 +439,7 @@ int main() {
 		re = maze.solve_newsetandcalall(iss1, iss2);
 		if (iss1)printf("%d\n", re);
 		else printf("Poor ANGEL has to stay in the prison all his life.\n");
+		maze.solve_plotpath(re);
 		maze.solve_del();
 		maze.del();
 	};
